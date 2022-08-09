320d0c8dcaaa96fc94501b01446369e9448dc4b5
Kseniia <ksenjadzjubina@gmail.com>
2022-08-09 23:34:30 +0300

    Добавлен первый вариант решения задачи

---
+++ Task001/Program.cs
@@ -0,0 +1,7 @@
+﻿int [] Numbers = {99, 100, 1, 0, 95, 574, 660, 1000, -5, -150, 5555, 7, 0, 0};
+for (int i = 0; i < Numbers.Length; i++)
+    
+        if (Numbers[i] < 100 && Numbers[i] > -100)
+           {
+            Console.Write(Numbers[i] + " ");
+           }
