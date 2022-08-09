a1458b46b7613c99f50d0771e8cdd04e1933131f
Kseniia <ksenjadzjubina@gmail.com>
2022-08-09 23:48:11 +0300

    Добавлен второй вариант решения задачи

---
+++ Task002/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] Row = { "Kazan", "Moscow", "Rome", "LA", "Ufa", "2", "897", "Pho", "0"};
+var result = new string[Row.Length];
+var Index = 0;
+foreach (var value in Row)
+{
+    if (value.Length <=3)
+    {
+        result[Index] = value;
+        Index++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, Index));
